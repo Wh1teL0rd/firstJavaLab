452ed380c5051580e7bcdc8d1165d479027dd6b8
Wh1teL0rd <pacman.oleg@gmail.com>
2023-04-18 12:13:33 +0300

    minor changes and codestyle fixes

--- src/main/java/ua/lviv/iot/algo/part1/lab1/DroneWriter.java
+++ src/main/java/ua/lviv/iot/algo/part1/lab1/DroneWriter.java
@@ -2,7 +2,9 @@ package ua.lviv.iot.algo.part1.lab1;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DroneWriter {
     public void writeToFile(List<Drone> drones, String fileName) throws IOException {
@@ -13,12 +15,7 @@ public class DroneWriter {
         FileWriter writer = new FileWriter(fileName);
         Map<String, Boolean> headerMap = new HashMap<>();
 
-        drones.sort(new Comparator<Drone>() {
-            @Override
-            public int compare(Drone d1, Drone d2) {
-                return d1.getClass().getSimpleName().compareTo(d2.getClass().getSimpleName());
-            }
-        });
+        drones.sort((d1, d2) -> d1.getClass().getSimpleName().compareTo(d2.getClass().getSimpleName()));
 
         for(Drone drone : drones){
             if(!headerMap.containsKey(drone.getClass().getSimpleName())){
